7226cacfe555fc231dee25376be05183fd9b3c8a
Ryan Guimarães <ryan.guimar10@gmail.com>
2025-07-23 23:47:59 -0300

    Commit inicial com estrutura de pastas e primeiro arquivo da primeira aula

---
+++ aula I/DemoPRimitivo.java
@@ -0,0 +1,27 @@
+
+public class DemoPrimitivo {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 1230;
+        int v = 12345;
+        long quantidade = 1234567891;
+        float taxaJuros = 12.2f;
+        double sineVal = 12345.23d;
+        boolean flag = true;
+        boolean val = false;
+        char ch1 = 88;
+        char ch2 = 'Y';
+        // tipos primitivos
+
+        System.out.println("imprimindo byte: " + b);
+        System.out.println("imprimindo short: " + s);
+        System.out.println("imprimindo int: " + v);
+        System.out.println("imprimindo long: " + quantidade);
+        System.out.println("imprimindo floar: " + taxaJuros);
+        System.out.println("imprimindo double: " + sineVal);
+        System.out.println("imprimindo boolean: " + flag);
+        System.out.println("imprimindo char: " + ch1);
+        
+
+    }
+}
